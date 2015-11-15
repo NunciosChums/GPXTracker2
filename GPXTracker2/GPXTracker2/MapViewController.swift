862bfa3dc73a67b17e9f2567a8155557b73778df
Changhoon <susemi99@gmail.com>
2015-11-15 23:22:40 +0900

    fix pin image error

--- GPXTracker2/GPXTracker2/MapViewController.swift
+++ GPXTracker2/GPXTracker2/MapViewController.swift
@@ -218,36 +218,41 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
       return nil
     }
     
-    var pinView: MKPinAnnotationView = MKPinAnnotationView()
-    
-    if let pin = annotation as? GTPin{
-      let reuseId = pin.identifier
-      
-      if let dequeuedView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
-      {
-        pinView = dequeuedView
-        pinView.annotation = annotation
-      }
-      else
-      {
-        pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
-        pinView.canShowCallout = true
-        pinView.animatesDrop = true
-        pinView.pinTintColor = pin.color
-        
-        let navigationButton = UIButton(type: .DetailDisclosure)
-        navigationButton.setImage(UIImage(named: "car"), forState: UIControlState.Normal)
-        pinView.leftCalloutAccessoryView = navigationButton
-      }
-      
-      if !(pin.iconUrl ?? "").isEmpty {
-        Alamofire.request(.GET, pin.iconUrl!).response() {(_, _, data, _) in
-          pinView.image = UIImage(data: data!)
-        }
-      }
+    if !(annotation is GTPin) {
+      return nil
     }
     
-    return pinView
+    let pin = annotation as? GTPin
+    let reuseId = pin!.identifier
+    
+    if (pin!.iconUrl ?? "").isEmpty
+    {
+      let pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+      pinView.canShowCallout = true
+      pinView.animatesDrop = true
+      pinView.pinTintColor = pin!.color
+      
+      let navigationButton = UIButton(type: .DetailDisclosure)
+      navigationButton.setImage(UIImage(named: "car"), forState: UIControlState.Normal)
+      pinView.leftCalloutAccessoryView = navigationButton
+      
+      return pinView
+    }
+    else
+    {
+      let pinView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+      pinView.canShowCallout = true
+      
+      let navigationButton = UIButton(type: .DetailDisclosure)
+      navigationButton.setImage(UIImage(named: "car"), forState: UIControlState.Normal)
+      pinView.leftCalloutAccessoryView = navigationButton
+      
+      Alamofire.request(.GET, pin!.iconUrl!).response() {(_, _, data, _) in
+        pinView.image = UIImage(data: data!)
+      }
+      
+      return pinView;
+    }
   }
   
   func mapView(mapView: MKMapView!, annotationView view: MKAnnotationView!, calloutAccessoryControlTapped control: UIControl!) {

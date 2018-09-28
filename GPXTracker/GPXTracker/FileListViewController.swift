21434fbd840023f169ec0acc274749bfe0e2f6b4
changhoon.jin <changhoon.jin@lgepartner.com>
2018-09-28 21:08:17 +0900

    Xcode 10.0

--- GPXTracker/GPXTracker/FileListViewController.swift
+++ GPXTracker/GPXTracker/FileListViewController.swift
@@ -64,7 +64,7 @@ class FileListViewController: UITableViewController {
     return cell
   }
   
-  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
     if editingStyle == .delete {
       let row = indexPath.row
       FileController.delete(file: self.items[row])
